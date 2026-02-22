2fb0a782d87fc01b16bb50a9b3e12b7d4675b56e
Leon Henning <leonhenning@protonmail.com>
2026-02-22 21:12:04 +0100

    hello_world

---
+++ c_programming_a_modern_approach/chapter_2/hello_world.c
@@ -0,0 +1,18 @@
+/*
+*Author: Leon Henning https://github.com/Leon-Henning-source
+*Date: 22.02.2026
+*Book: C Programming - A Modern Approach second edition by K. N. King
+*Chapter: 2
+*Exercise: 1
+*Program Name: hello_world
+*Purpose: Create and run Kernighan and Ritchie's 
+*famous "hello, world" program
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world\n");
+	return 0;
+}
